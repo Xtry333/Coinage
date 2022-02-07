8853a39af5a60b8461194349a023231e24107386
Michał Woźniak <xtry333@gmail.com>
2022-02-07 22:06:15 +0100

    yarn update

--- apps/coinage/src/app/pipes/pln-currency.pipe.ts
+++ apps/coinage/src/app/pipes/pln-currency.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PlnCurrencyPipe implements PipeTransform {
     public readonly CURRENCY_SYMBOL = 'zł';
     transform(value: number | null): string {
-        if (!value) {
+        if (value === null) {
             return '- ' + this.CURRENCY_SYMBOL;
         }
         return value.toFixed(2).replace('.', ',') + ' ' + this.CURRENCY_SYMBOL;
