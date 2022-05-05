b285d48be518433fd9f23fa80af0cf4cae4b6a57
Michał Woźniak <xtry333@gmail.com>
2022-05-05 23:16:49 +0200

    yarn update

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
     displayName: 'api',
-    preset: '../../jest.preset.js',
+
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,4 +11,5 @@ module.exports = {
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageDirectory: '../../coverage/apps/api',
+    preset: '../../jest.preset.ts',
 };
