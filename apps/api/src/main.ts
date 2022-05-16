41672f8319801497c0e1d25c5029c5d402f9ed67
Michał Woźniak <xtry333@gmail.com>
2022-05-16 17:29:35 +0200

    Adding helmet and update

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,25 +7,20 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app/app.module';
 import { NestFactory } from '@nestjs/core';
-
-declare const module: any;
+import helmet from 'helmet';
 
 async function bootstrap() {
     const globalPrefix = 'api';
     const port = process.env.PORT || 3333;
     const app = await NestFactory.create(AppModule);
 
+    app.use(helmet());
     app.setGlobalPrefix(globalPrefix);
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.enableCors();
     await app.listen(port, () => {
         Logger.log(`🚀 Api is running on: http://localhost:${port}/${globalPrefix}`, 'ServerBootstrap');
     });
-
-    if (module.hot) {
-        module.hot.accept();
-        module.hot.dispose(() => app.close());
-    }
 }
 
 bootstrap();
