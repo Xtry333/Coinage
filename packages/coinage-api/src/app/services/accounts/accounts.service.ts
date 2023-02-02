3addced37329a6930ba6192f121fba4e615bed9b
Michał Woźniak <xtry333@gmail.com>
2023-02-02 01:02:00 +0100

    fix

--- packages/coinage-api/src/app/services/accounts/accounts.service.ts
+++ packages/coinage-api/src/app/services/accounts/accounts.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { AccountDao } from '../../daos/account.dao';
-import { createMockAccounts } from '../../../test/mock-generators/accounts.mock';
 
 @Injectable()
 export class AccountsService {
     public constructor(private readonly accountDao: AccountDao) {}
 
     public getForUserId(userId: number) {
-        console.log(createMockAccounts(3))
         return this.accountDao.getForUserId(userId);
     }
 }
